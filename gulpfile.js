733d7b1e9370b5f808f932bc0b12cdeac0c201ae
realAbstractionist <justforsignup@mail.ru>
2016-01-27 14:33:06 +0700

    Init

---
+++ gulpfile.js
@@ -0,0 +1,8 @@
+var gulp = require('gulp');
+var gzip = require('gulp-gzip');
+ 
+//gulp.task('compress', function() {
+//  gulp.src('css/*.css')
+//    .pipe(gzip())
+//    .pipe(gulp.dest('build/'));
+//});
